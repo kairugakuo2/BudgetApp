1d4219070ba93a33043dc23345fda47b43cc4ae3
Gakuo <kairugakuo9@gmail.com>
2024-10-19 18:49:18 -0500

    Updated ExpenseManager.java with method to move expenses from csv file to arraylist

--- ExpenseManager.java
+++ ExpenseManager.java
@@ -14,3 +14,22 @@ public static void saveExpense(Expense expense) throws IOException {
         String line = reader.readLine();
     }
 }
+
+// move expenses from csv file to list in program
+
+public static List<Expense> readExpense() throws IOException{
+    List<Expense> expenses = new ArrayList<>(); //list to store expense variables
+    try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+
+        String line = reader.readLine();
+        while(line != null) {
+            String[] parts = line.split(",");
+            double amount = Double.parseDouble(parts[0]);
+            String category = parts[1];
+            LocalDate date = LocalDate.parse(parts[2], formatter);
+            String description = parts[3];
+            expenses.add(new Expense(amount, category, date, description));
+        }
+        return expenses;
+    }
+}
