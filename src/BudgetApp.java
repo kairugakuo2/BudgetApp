ef04ed54388bfbd0e15bb48ba1603d0193962020
Gakuo <kairugakuo9@gmail.com>
2024-10-28 19:22:04 -0500

    Updated displayBudget method to display a budget summary by calling other methods and throwing an IO exception.

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -3,9 +3,23 @@ import java.util.*;
 
 public class BudgetApp{
 
+    // log a new expense and save to csv file
     public static void logExpense(double amount, String category, String description){
         LocalDate today = LocalDate.now();
         Expense newExpense = new Expense(amount, category, today, description);
         ExpenseManager.saveExpense(newExpense);
+    }
+
+    // display budget summary (remaining budget, total expenses, daily spending limit)
+    public static void displayBudget(double totalBudget) throws IOException{
+        List<Expense> expenses = ExpenseManager.readExpense(); // read and load all expenses from csv file
+        double totalExpenses = BudgetCalculator.totalExpenses(expenses);
+        double remainingBudget = BudgetCalculator.remainingBudget(totalBudget, expenses);
+        double dailyLimit = BudgetCalculator.dailyLimit(remainingBudget);
+
+        //print all of the info
+        System.out.println("Total expenses: $" + totalExpenses);
+        System.out.println("Remaining expenses: $" + remainingBudget);
+        System.out.println("Daily limit: $" + dailyLimit);
     }
 }
